e86fd5b1b4f9d71affe4772b195a79e9137d6cd0
libbylondon <libbylondon22@gmail.com>
2016-09-18 18:08:57 -0500

    lmost ready

--- app.js
+++ app.js
@@ -1,40 +1,5 @@
-// $(function(){
-//   $("#myList").on("submit", function(event){
-//       event.preventDefault();
-//       //console.log("it works");
-//       var list = $("#myList").serializeArray();
-//       // console.log(list);
-//       // console.log(list[0]);
-//       // console.log(list[0].value);
-// $("#employeeDiv").append("<ul></ul>")
-//       for(var it = 0; it <list.length; it ++){
-//         $("ul").append("<br>" + list[it].value)
-//       }
-//       // $("ul").append();
-//       // $(".removeButton").on("click", function(){
-//       //   $(this).parent().parent().remove();
-//       // })
-// // console.log(event);//inactive due to console log below
-//
-//     });
-//     $( '#employeeDiv' ).each(function(){
-//         this.reset();
-//     });
-// })
-// function addListItem() {
-//     var textToAdd = $('.input').val();                    // The finish class is just for css styling
-//     $('#list').append(textToAdd + '<button class="delete">Delete</button>');
-//     $('.input').val('');
-// }
-//
-// function deleteItem() {
-//     $(this).parent().remove();
-// }
-// $( ".input" ).append();
 $(document).ready(function() {
-//     $('#add').on('click', addListItem);
-//     $(document).on('click', '.delete', deleteItem);
-// });
+
 $('#submit').click(function() {
 	var firstName = $("#firstName").val();
   var lastName = $("#lastName").val();
@@ -46,6 +11,6 @@ $('#submit').click(function() {
   var additional = $("#additional").val();
 
 
-	$("#content ul").append('<li> First Name: ' + firstName + ' Last Name: ' + lastName + ' Email Address: ' + emailAddress + ' Company: ' + company + ' Title: ' + title + ', Country: ' + country + '  State: ' + state + ' Additional Comments? ' + additional + ' </li');
+	$("#content ul").append('<li> First Name ' + firstName + ' Last Name ' + lastName + ' Email Address ' + emailAddress + ' Company ' + company + ' Title ' + title + ', Country ' + country + '  State ' + state + ' Additional Comments? ' + additional + ' </li');
 });
 })
